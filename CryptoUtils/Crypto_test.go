065e81961f0a41ab3f1683c999a71950a5b4bb1e
Jason Ren <40999116+jasonren0403@users.noreply.github.com>
2021-05-02 19:12:41 +0800

    Method for AES algorithm & tests of them

--- CryptoUtils/Crypto_test.go
+++ CryptoUtils/Crypto_test.go
@@ -1,3 +1,48 @@
 package CryptoUtils
 
+import (
+	"github.com/stretchr/testify/assert"
+	"log"
+	"testing"
+)
+
 /* Crypto_test.go -- Test if crypto function work correctly */
+
+func TestAESByB64(t *testing.T) {
+	key := []byte("testKey1testKey1")
+	plaintxt := "aesEncTest"
+	err, val := DoAESEncrypt(key, plaintxt, USE_BASE64)
+	if assert.NoError(t, err, "Encrypt should not cause error") {
+		assert.NotEmpty(t, val, "cipher should not be empty")
+		log.Printf("Enc value = %s", val)
+	}
+	log.Printf("Decrypt '%s' --> (key=%s)", val, key)
+	err, val2 := DoAESDecrypt(key, "thbBaJFTT1LmGg", USE_BASE64)
+	if assert.NoError(t, err, "Decrypt should not cause error") {
+		assert.NotEmpty(t, val2, "cipher should not be empty")
+		assert.EqualValues(t, plaintxt, val2, "Decrypt should be equal to original plaintext")
+	}
+}
+
+func TestAESByHex(t *testing.T) {
+	key := []byte("testKey1testKey1")
+	plaintxt := "aesEncTest"
+	err, val := DoAESEncrypt(key, plaintxt, USE_HEX)
+	if assert.NoError(t, err, "Encrypt should not cause error") {
+		assert.NotEmpty(t, val, "cipher should not be empty")
+		log.Printf("Enc value = %s", val)
+	}
+	log.Printf("Decrypt '%s' --> (key=%s)", val, key)
+	err, val2 := DoAESDecrypt(key, "b616c16891534f52e61a", USE_HEX)
+	if assert.NoError(t, err, "Decrypt should not cause error") {
+		assert.NotEmpty(t, val2, "cipher should not be empty")
+		assert.EqualValues(t, plaintxt, val2, "Decrypt should be equal to original plaintext")
+	}
+}
+
+func TestAESEdgeCase(t *testing.T) {
+	key1 := []byte("Not Valid key length")
+	err, _ := DoAESEncrypt(key1, "lllllllllllllllllllllllllllllllllllllll", USE_BASE64)
+	assert.Error(t, err, "AES should only use key length 16/24/32, this key's length is", len(key1),
+		"so it should not be received")
+}
--- CryptoUtils/api.go
+++ CryptoUtils/api.go
@@ -1,25 +1,79 @@
 package CryptoUtils
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
 	"encoding/hex"
+	"errors"
+	"fmt"
 )
 
-func DoAESEncrypt(key []byte, plaintext string) []byte {
-	return nil //todo
-}
+type CryptMode int8
 
-func DoAESDecrypt(key []byte, ciphertext []byte) string {
-	return "" //todo
-}
+const (
+	OFB        CryptMode = 0x1
+	CBC        CryptMode = 0x2
+	CTR        CryptMode = 0x3
+	USE_BASE64 CryptMode = 0x10
+	USE_HEX    CryptMode = 0x11
+)
 
-func toHexString(src []byte) string {
-	return hex.EncodeToString(src)
-}
+// DoAESEncrypt does an OFB style encryption to given text
+// key[:blockSize] as IV
+func DoAESEncrypt(key []byte, plaintext string, c CryptMode) (error, string) {
+	plaindata := []byte(plaintext)
 
-func hexToByte(src string) []byte {
-	decoded, err := hex.DecodeString(src)
+	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return errors.New(fmt.Sprintf("key 长度必须 16/24/32长度: %s", err.Error())), ""
 	}
-	return decoded
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewOFB(block, key[:blockSize])
+	crypted := make([]byte, len(plaindata))
+	blockMode.XORKeyStream(crypted, plaindata)
+	switch c ^ 0x10 {
+	case 0:
+		return nil, base64.RawURLEncoding.EncodeToString(crypted)
+	case 1:
+		return nil, hex.EncodeToString(crypted)
+	default:
+		return errors.New("cannot reach here, bad c value"), ""
+	}
+}
+
+// DoAESDecrypt does an OFB style decryption to given text
+// key[:blockSize] as IV
+func DoAESDecrypt(key []byte, ciphertext string, c CryptMode) (error, string) {
+	var cryptedByte []byte
+	switch c ^ 0x10 {
+	case 0:
+		cryptedByte, _ = base64.RawURLEncoding.DecodeString(ciphertext)
+	case 1:
+		cryptedByte, _ = hex.DecodeString(ciphertext)
+	default:
+		return errors.New("cannot reach here, bad c value"), ""
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return errors.New(fmt.Sprintf("key 长度必须 16/24/32长度: %s", err.Error())), ""
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewOFB(block, key[:blockSize])
+	orig := make([]byte, len(cryptedByte))
+	blockMode.XORKeyStream(orig, cryptedByte)
+	return nil, string(orig)
+}
+
+func PKCS7Padding(ciphertext []byte, blocksize int) []byte {
+	padding := blocksize - len(ciphertext)%blocksize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func PKCS7UnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
 }
